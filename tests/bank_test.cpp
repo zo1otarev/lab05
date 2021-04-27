5e2c1d0f8220fd9af23deee57ee4fefc42ef8dff
SovesT1337 <temirkhan2002@gmail.com>
2021-04-28 00:11:09 +0300

    I want to sleep

--- tests/bank_test.cpp
+++ tests/bank_test.cpp
@@ -10,7 +10,7 @@
   class MockAccount: public Account{
   public:
    MockAccount(int id, int balance):Account(id, balance){}
-   MOCK_METHOD(int, GetBalance, (), (const, override));
+   MOCK_METHOD(int GetBalance () (const, override));
    MOCK_METHOD(void, ChangeBalance, (int diff), (override));
    MOCK_METHOD(void, Lock, (), (override));
    MOCK_METHOD(void, Unlock, (), (override));
