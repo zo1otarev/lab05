61165cd541c60915db0c600054def9a04d638945
Zolotarev Daniil <zolotarew.daniil@mail.ru>
2021-05-12 01:25:51 +0300

    Just new commit

---
+++ cmake-build-debug/banking/CMakeFiles/banking.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daniil/timp_lab05/banking/Account.cpp" "/home/daniil/timp_lab05/cmake-build-debug/banking/CMakeFiles/banking.dir/Account.cpp.o"
+  "/home/daniil/timp_lab05/banking/Transaction.cpp" "/home/daniil/timp_lab05/cmake-build-debug/banking/CMakeFiles/banking.dir/Transaction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../banking"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/banking/CMakeFiles/banking.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/banking.dir/Account.cpp.o"
+  "CMakeFiles/banking.dir/Transaction.cpp.o"
+  "libbanking.a"
+  "libbanking.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/banking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
